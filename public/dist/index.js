afce2ebf0cc5cccad8c8a41b1ef66ad1f981cd0d
zporange <253149415@qq.com>
2017-04-06 07:39:13 +0800

    lesson5

---
+++ public/dist/index.js
@@ -0,0 +1,44 @@
+webpackJsonp([0],{
+
+/***/ 2:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function($) {
+
+setTimeout(function () {
+  $.ajax({
+    url: '/user.action',
+    method: 'get',
+    success: function success(arr) {
+      var liStr = arr.map(function (ele) {
+        return '<li>' + ele + '</li>';
+      }).join('');
+      $('#root').html(liStr);
+    },
+    error: function error(err) {
+      console.log(err);
+    }
+  });
+  // 模拟 post
+  $.ajax({
+    url: '/list.action',
+    method: 'post',
+    data: JSON.stringify(["name", "jrg"]),
+    success: function success(arr) {
+      var liStr = arr.map(function (ele) {
+        return '<li>' + ele + '</li>';
+      }).join('');
+      $('#shop').html(liStr);
+    },
+    error: function error(err) {
+      console.log(err);
+    }
+  });
+}, 1000);
+/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
+
+/***/ })
+
+},[2]);
+//# sourceMappingURL=index.js.map
